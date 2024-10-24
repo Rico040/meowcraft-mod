bf418853d0f2442c1e9934cc7b3c8d6aaaa297de
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-10-24 23:29:23 +1000

    Initial commit

---
+++ src/main/java/su/rico040/meowcraft/MeowcraftClient.java
@@ -0,0 +1,10 @@
+package su.rico040.meowcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MeowcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/su/rico040/meowcraft/MeowcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package su.rico040.meowcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MeowcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
